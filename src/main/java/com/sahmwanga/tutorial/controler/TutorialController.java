153939b41547cbb4a6e1109a8adf6a6d85f22100
Salehe <sahmwanga@gmail.com>
2023-09-13 15:52:04 +0300

    Added pagination

--- src/main/java/com/sahmwanga/tutorial/controler/TutorialController.java
+++ src/main/java/com/sahmwanga/tutorial/controler/TutorialController.java
@@ -1,10 +1,15 @@
 package com.sahmwanga.tutorial.controler;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -30,13 +35,26 @@ public class TutorialController {
     TutorialRepository tutorialRepository;
 
     @GetMapping("/tutorials")
-    public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title) {
+    public ResponseEntity<Map<String, Object>> getAllTutorials(@RequestParam(required = false) String title,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "3") int size) {
         try {
             List<Tutorial> tutorials = new ArrayList<Tutorial>();
+            Pageable paging = PageRequest.of(page, size);
+
+            Page<Tutorial> pageTuts;
             if (title == null)
-                tutorialRepository.findAll().forEach(tutorials::add);
+                pageTuts = tutorialRepository.findAll(paging);
             else
-                tutorialRepository.findByTitleContaining(title).forEach(tutorials::add);
+                pageTuts = tutorialRepository.findByTitleContaining(title, paging);
+
+            tutorials = pageTuts.getContent();
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("tutorials", tutorials);
+            response.put("currentPage", pageTuts.getNumber());
+            response.put("totalItems", pageTuts.getTotalElements());
+            response.put("totalPages", pageTuts.getTotalPages());
 
             if (tutorials.isEmpty()) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
@@ -44,7 +62,7 @@ public class TutorialController {
 
             System.out.println(tutorials);
 
-            return new ResponseEntity<>(tutorials, HttpStatus.OK);
+            return new ResponseEntity<>(response, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
@@ -110,14 +128,28 @@ public class TutorialController {
     }
 
     @GetMapping("/tutorials/published")
-    public ResponseEntity<List<Tutorial>> findByPublished() {
+    public ResponseEntity<Map<String, Object>> findByPublished(@RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "3") int size) {
         try {
-            List<Tutorial> tutorials = tutorialRepository.findByPublished(true);
+
+            List<Tutorial> tutorials = new ArrayList<Tutorial>();
+
+            Pageable paging = PageRequest.of(page, size);
+            Page<Tutorial> pageTuts = tutorialRepository.findByPublished(true, paging);
+
+            tutorials = pageTuts.getContent();
 
             if (tutorials.isEmpty()) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
-            return new ResponseEntity<>(tutorials, HttpStatus.OK);
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("tutorials", tutorials);
+            response.put("currentPage", pageTuts.getNumber());
+            response.put("totalItems", pageTuts.getTotalElements());
+            response.put("totalPages", pageTuts.getTotalPages());
+
+            return new ResponseEntity<>(response, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
--- src/main/java/com/sahmwanga/tutorial/repository/TutorialRepository.java
+++ src/main/java/com/sahmwanga/tutorial/repository/TutorialRepository.java
@@ -2,11 +2,13 @@ package com.sahmwanga.tutorial.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.sahmwanga.tutorial.model.Tutorial;
 
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
-    List<Tutorial> findByPublished(boolean published);
+    Page<Tutorial> findByPublished(boolean published, Pageable pageable);
 
-    List<Tutorial> findByTitleContaining(String title);
+    Page<Tutorial> findByTitleContaining(String title, Pageable pageable);
 }
